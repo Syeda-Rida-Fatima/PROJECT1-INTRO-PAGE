deecd5311657298ea31ac113d47c7e262147c274
Rida Fatima <syedaridafatima67@gmail.com>
2023-05-31 19:41:23 +0500

    add necessary css-tailwind

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
 import Button from './Button';
 
-
 function App() {
+  const handleClick = () => {
+    console.log('Clicked!');
+  };
+
   return (
     <div>
       <div>
-        <Button>buy now</Button>
+        <Button
+          secondary
+          outline
+          rounded
+          className="mb-5"
+          onClick={handleClick}
+        >
+          <GoBell />
+          Click me!!
+        </Button>
       </div>
       <div>
-        <Button>see deal</Button>
+        <Button danger outline onMouseEnter={handleClick}>
+          <GoCloudDownload />
+          Buy Now!
+        </Button>
       </div>
       <div>
-        <Button>hii</Button>
+        <Button warning onMouseLeave={handleClick}>
+          <GoDatabase />
+          See Deal!
+        </Button>
       </div>
       <div>
-        <Button></Button>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
       </div>
-    </div>)
+      <div>
+        <Button primary rounded>
+          Something!
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
